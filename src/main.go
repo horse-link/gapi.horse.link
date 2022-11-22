036186e80362beddb1506fe457c803eed4816679
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-11-22 15:01:43 +1000

    chore: call tab

--- src/main.go
+++ src/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
+
+var client *http.Client
 
 type Runner struct {
 	Nonce    int    `json:"nonce"`
@@ -26,16 +32,27 @@ type Runner struct {
 // }
 
 func getRunners(context *gin.Context) {
-	var runners = []Runner{
-		{	Nonce: 1,
-			Number:        1,
-			Name:          "Metaverse",
-			MarketId:      "1",
-			Close:         1,
-			End:           1,
-			PropositionId: "1",
-		},
+
+	url:= "https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/2022-11-22/meetings/R/WAG/races/5?jurisdiction=QLD&returnPromo=false"
+
+	var runners []Runner
+
+	err := GetJson(url, runners)
+	if err != nil {
+		fmt.Println(err)
 	}
+
+	// var runners = []Runner{
+	// 	{	Nonce: 1,
+	// 		Number:        1,
+	// 		Name:          "Metaverse",
+	// 		MarketId:      "1",
+	// 		Close:         1,
+	// 		End:           1,
+	// 		PropositionId: "1",
+	// 	},
+	// }
+
 	context.IndentedJSON(http.StatusOK, runners)
 
 	// if err := ; err != nil {
@@ -45,7 +62,18 @@ func getRunners(context *gin.Context) {
 	// context.JSON(http.StatusOK, gin.H{"data": runners})
 }
 
+func GetJson(url string, target interface{}) error {
+	r, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
 func main() {
+	client = &http.Client{Timeout: 10 * time.Second}
 	router := gin.Default()
 
 	// router.GET("/runners", func(c *gin.Context) {
